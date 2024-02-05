9b60508342a69df0ed040e2ed3db7a62e8e03678
IvanTulin <tulin.v@bk.ru>
2024-02-05 16:53:13 +0300

    init

---
+++ UIKitTeamPlate/UIKitTeamPlate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTeamPlate
+//
+//  Created by Ivan Tulin on 05.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
