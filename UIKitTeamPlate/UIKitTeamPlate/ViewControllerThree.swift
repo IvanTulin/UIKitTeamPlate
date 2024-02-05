eeb4e5ed66f57fcf092ef3df11a4040996daebb5
IvanTulin <tulin.v@bk.ru>
2024-02-05 17:57:43 +0300

    created TestApp2

---
+++ UIKitTeamPlate/UIKitTeamPlate/ViewControllerThree.swift
@@ -0,0 +1,20 @@
+//
+//  ViewControllerThree.swift
+//  UIKitTeamPlate
+//
+//  Created by Ivan Tulin on 05.02.2024.
+//
+
+import UIKit
+
+class ViewControllerThree: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
