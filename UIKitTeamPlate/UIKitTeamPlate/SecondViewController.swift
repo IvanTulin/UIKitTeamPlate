dfaf53c806cc81b6cdca3b9a2ff8dda3dc56a037
IvanTulin <tulin.v@bk.ru>
2024-02-06 23:00:33 +0300

    add swift lint

---
+++ UIKitTeamPlate/UIKitTeamPlate/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  UIKitTeamPlate
+//
+//  Created by Ivan Tulin on 06.02.2024.
+//
+
+import UIKit
+/// второй экран
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
--- UIKitTeamPlate/UIKitTeamPlate/ViewController.swift
+++ UIKitTeamPlate/UIKitTeamPlate/ViewController.swift
@@ -6,14 +6,11 @@
 //
 
 import UIKit
-
+/// старотовый экран
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
-
