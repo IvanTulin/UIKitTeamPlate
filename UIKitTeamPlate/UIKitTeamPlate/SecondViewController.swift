acc57e0f33b6cb8d6540e39269a8f254a1a52108
IvanTulin <tulin.v@bk.ru>
2024-02-06 16:30:00 +0300

    add Format

--- UIKitTeamPlate/UIKitTeamPlate/SecondViewController.swift
+++ UIKitTeamPlate/UIKitTeamPlate/SecondViewController.swift
@@ -1,14 +1,10 @@
-//
-//  SecondViewController.swift
-//  UIKitTeamPlate
-//
-//  Created by Ivan Tulin on 06.02.2024.
-//
+// SecondViewController.swift
+// Copyright © RoadMap. All rights reserved.
 
 import UIKit
+
 /// класс SecondViewController
 class SecondViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
--- UIKitTeamPlate/UIKitTeamPlate/ViewController.swift
+++ UIKitTeamPlate/UIKitTeamPlate/ViewController.swift
@@ -1,14 +1,10 @@
-//
-//  ViewController.swift
-//  UIKitTeamPlate
-//
-//  Created by Ivan Tulin on 05.02.2024.
-//
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
 
 import UIKit
+
 /// класс ViewController
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
