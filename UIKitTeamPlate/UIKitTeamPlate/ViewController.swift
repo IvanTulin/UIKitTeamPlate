90e8c708618b8973108d3f3898ac7785073caf5f
IvanTulin <tulin.v@bk.ru>
2024-02-19 17:26:02 +0300

    Создал проэкт RM Link

--- UIKitTeamPlate/UIKitTeamPlate/ViewController.swift
+++ UIKitTeamPlate/UIKitTeamPlate/ViewController.swift
@@ -7,6 +7,10 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .red
+    }
+    
+    private func configure() {
+        
     }
 }
